9f106e2423a5081a8fbf5858a074bb06b308295c
kinggatsheni <codedev95@outlook.com>
2021-09-27 16:39:57 +0200

    update

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using OnlineShop.Models;
+using System;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace OnlineShop.Controllers
 {
     public class ProfileController : Controller
     {
         // GET: Profile
+        ApplicationDbContext context;
+
+        public ProfileController()
+        {
+            this.context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(Customer customer) {
+            var profile = new Customer() {
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Email = User.Identity.GetUserName(),
+                CellNo = customer.CellNo,
+                Address = customer.Address,
+                ApplicationUserId = User.Identity.GetUserId(),
+            };
+
+            context.Customers.Add(profile);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
     }
 }
